2c673ccd01b2b6ebd26e1d86bd8ed14d1b85e930
Pranav.Maheshwary <pranavmaheshwary005@gmail.com>
2019-07-29 14:44:12 +0530

    Adding interface and related classes for static analysis tools

---
+++ StaticAnalyzer/StaticAnalyzer/Ndepend.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticAnalyzer
+{
+    class Ndepend : StaticAnalysisTool
+    {
+        public void prepareInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void processOutput()
+        {
+            throw new NotImplementedException();
+        }
+
+        vo
+    }
+}
