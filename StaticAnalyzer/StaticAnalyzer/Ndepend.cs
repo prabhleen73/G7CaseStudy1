5783b0da4a977d086777290e49164715c34744f1
umesh1639 <singhumesh3008@gmail.com>
2019-07-30 15:43:41 +0530

    Update Ndepend.cs

--- StaticAnalyzer/StaticAnalyzer/Ndepend.cs
+++ StaticAnalyzer/StaticAnalyzer/Ndepend.cs
@@ -8,10 +8,11 @@ namespace StaticAnalyzer
 {
     class Ndepend : StaticAnalysisTool
     {
+         string xmloutpath=" ";//initialize this to xmloutpath
         public bool prepareInput(string[] filename)
         {
             int i = 0;
-            while (filename[i])
+            while (filename[i]!=null)
             {
                 XElement root = XElement.Load(@"C:\Users\320069097\source\repos\G7CaseStudy1-prev\StaticAnalysisTool\StaticAnalysisTool.ndproj");
                 XElement requiredVal = (from elem in root.DescendantsAndSelf()
@@ -22,15 +23,17 @@ namespace StaticAnalyzer
                     Console.WriteLine(requiredVal.Attribute("FilePath").Value);
                 }
                 requiredVal.Attribute("FilePath").SetValue(@"C:\Users\320069097\source\repos\G7CaseStudy1\StaticAnalysisTool\" +filename[i]+ ".sln");
+               
                 root.Save(@"C:\Users\320069097\source\repos\G7CaseStudy1-prev\StaticAnalysisTool\StaticAnalysisTool.ndproj");
                 i++;
             }
 
-            
+            return true;
         }
 
         public void processOutput()
         {
+            string xmllocation="NDependTrendData2019.xml";//initialize this to xmloutpath
             ParsingXmlNDepend.ShowingResultsAfterParsingNDependXml(xmllocation);
             throw new NotImplementedException();
         }
